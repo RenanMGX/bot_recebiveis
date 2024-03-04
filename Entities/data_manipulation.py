49e9a5abd0e26229f4e47d44ff9565500ee3420c
Renan Brian Hermenegildo Oliveira <renan.oliveira@patrimar.com.br>
2024-03-04 18:48:32 -0300

    corrigindo bug salvando excel

--- Entities/data_manipulation.py
+++ Entities/data_manipulation.py
@@ -18,19 +18,48 @@ class XWtoDF:
             
             for sheet_name in wb.sheet_names:
                 sheet = wb.sheets[sheet_name]
+                
+                
                 data: pd.DataFrame = sheet.range('A1').expand().options(pd.DataFrame, index=False, header=True).value
+                #data: pd.DataFrame = sheet.range('A1:AD1').expand('down').options(pd.DataFrame, index=False, header=True).value
                 
                 # for coluna in data.columns:
                 #     if pd.api.types.is_datetime64_any_dtype(data[coluna]):
                 #         data[coluna] = data[coluna].dt.to_pydatetime()
-                
-                df[sheet_name] = data
+                df[sheet_name] = data.replace(float('nan'), "")
+                #import pdb; pdb.set_trace()
         
         for app_open in xw.apps:
-            if xw.books[0].name == path.split("\\")[-1]:
+            if app_open.books[0].name == path.split("\\")[-1]:
+                app_open.kill()
+            elif app_open.books[0].name == 'Pasta1':
                 app_open.kill()
         
         return df
+    
+    def save_excel(*, path:str, df:pd.DataFrame, sheet_name_to_save:str) -> None:
+        app = xw.App(visible=False)
+        dados_para_adicionar:list = df.values.tolist()
+        dados_para_adicionar.insert(0, df.columns.tolist())
+        
+        solicitacao = [x for x in df['Solicitação']]
+        solicitacao.insert(0, 'Solicitação')
+        
+        with app.books.open(path)as wb:
+            for sheet_name in wb.sheet_names:
+                if sheet_name_to_save in sheet_name:
+                    sheet = wb.sheets[wb.sheet_names.index(sheet_name)]
+                    for x in range(len(solicitacao)): sheet.range(f'N{x+1}').value = solicitacao[x]
+                    #import pdb; pdb.set_trace()
+                    
+                    #sheet.range('A1').expand().value = dados_para_adicionar
+            wb.save()
+        
+        for x in xw.apps:
+            if x.books[0].name == path.split("\\")[-1]:
+                x.kill()
+            elif x.books[0].name == 'Pasta1':
+                x.kill()
 
 if __name__ == "__main__":
     pass
--- main.py
+++ main.py
@@ -4,10 +4,17 @@ from Entities.credencital_load import Credential
 from typing import Dict, List, Hashable, Any
 import pandas as pd
 import traceback
+import xlwings as xw # type: ignore
 
 if __name__ == "__main__":
     #carregar aquivo
-    path = "#materiais\\planilha_teste.xlsx"
+    path = "#materiais\\Base - Lançamento Juros de Obra3.xlsx"
+    
+    for app_open in xw.apps:
+        if app_open.books[0].name == path.split("\\")[-1]:
+            app_open.kill()
+        elif app_open.books[0].name == 'Pasta1':
+            app_open.kill()
     
     #ler planilha
     data_with_sheet:Dict[str, pd.DataFrame] = XWtoDF.read_excel(path)
@@ -16,15 +23,25 @@ if __name__ == "__main__":
     for sheet in data_with_sheet.keys():
         if 'pagamento' in sheet.lower():
             novos_pagamentos: pd.DataFrame = data_with_sheet[sheet]
-        elif 'contratos' in sheet.lower():
+        if 'contratos' in sheet.lower():
+            #import pdb; pdb.set_trace()
+            #df_contratos_temp = data_with_sheet[sheet]
+            #df_contratos_temp = df_contratos_temp[df_contratos_temp['Solicitação'] == ""]
             novos_contratos:List[dict] = data_with_sheet[sheet].to_dict(orient="records")
+            
     
+    
+    
+    #import pdb; pdb.set_trace()
     credencial = Credential.load("imbme_credential.json")
     #http://qas.patrimarengenharia.imobme.com/
     #https://patrimarengenharia.imobme.com/
     bot_navegador = ImobmeBot(user=credencial['user'], password=credencial['password'], url="http://qas.patrimarengenharia.imobme.com/")
     
     for dados in novos_contratos: # type: ignore
+        if dados['Solicitação'] != "":
+            print("passou")
+            continue
         print(f"{dados['Empreendimento']=}, {dados['Bloco']=}, {dados['Unidade']=} : Iniciado")
         try:
             codigo = bot_navegador.executar_contratos(url="http://qas.patrimarengenharia.imobme.com/Contrato/", dados=dados)
@@ -35,8 +52,12 @@ if __name__ == "__main__":
             print("        Error!")
             continue
     
-    novos_contratos = pd.DataFrame(novos_contratos)    # type: ignore
-    print(novos_contratos['Solicitação'])# type: ignore
+    novos_contratos_novo: pd.DataFrame = pd.DataFrame(novos_contratos) 
+    print(novos_contratos_novo['Solicitação'])
+    
+    XWtoDF.save_excel(path=path, df=novos_contratos_novo, sheet_name_to_save='contratos')
+    
+    #import pdb; pdb.set_trace()
     
     
     
